d1df70f64d6896a7b995d07af85d62e3d38319f6
pamgbet <pawel1.zych@gmail.com>
2020-06-09 19:42:25 +0200

    Filter out authors not from WUT

--- src/graph_context.py
+++ src/graph_context.py
@@ -7,6 +7,7 @@ from itertools import combinations
 class GraphContext:
     def __init__(self, path):
         self.original_df = pd.read_csv(path, delimiter=',')
+        self._wut_authors = self._get_wut_authors()
         self._cleaned_df = self._get_cleaned_dataset()
         self._publications_graph = None
         self._co_authorship_graph = None
@@ -15,8 +16,20 @@ class GraphContext:
     def _get_cleaned_dataset(self):
         cleaned_publications = pd.concat([pd.Series(row['Document Title'], row['Authors'].split('; '))
                                           for _, row in self.original_df.iterrows()]).reset_index()
+
         cleaned_publications.columns = ['Author', 'Title']
-        return cleaned_publications
+        # filter out authors not from WUT
+        filtered_publications = cleaned_publications[cleaned_publications["Author"].isin(self._wut_authors)]
+        return filtered_publications
+
+    def _get_wut_authors(self):
+        wut_affiliation = "Warsaw University of Technology"
+        authors_affiliations_mapper = {}
+        for _, row in self.original_df.iterrows():
+            authors_affiliations_mapper.update(dict(zip(list(map(lambda x: x.strip(), row['Authors'].split(';'))),
+                                             row['Author Affiliations'].split(';'))))
+        return {author for author, affiliations in authors_affiliations_mapper.items()
+                if wut_affiliation in affiliations}
 
     @property
     def raw_data(self):
